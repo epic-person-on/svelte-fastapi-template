9ad041e472a006c1284b37727dcdb613617bb452
Me <123142178+epic-person-on@users.noreply.github.com>
2025-08-17 19:42:15 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI, Depends
 from fastapi_throttle import RateLimiter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Limit to 5 requests per minute
-limiter = RateLimiter(times=5, seconds=60)
+# Deal with cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Limit to 5 request per second
+limiter = RateLimiter(times=5, seconds=1)
 
 @app.get("/", dependencies=[Depends(limiter)])
 async def root():
